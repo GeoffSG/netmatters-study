55ca24913c555f910f4f31f46216419bd94ae435
Geoff G <Geoff17391@gmail.com>
2024-01-08 10:37:03 +0000

    Converted banner from Slick to Owl
    Also styled the dots to match the original

--- js/components/banner.js
+++ js/components/banner.js
@@ -1,10 +1,19 @@
 function banner() {
     console.log("banner");
-    $(".banner").slick({
-        autoplay: true,
-        autoplaySpeed: 5000,
+    // $(".banner").slick({
+    //     autoplay: true,
+    //     autoplaySpeed: 5000,
+    //     dots: true,
+    //     arrows: false,
+    // });
+    $(".banner").owlCarousel({
+        items: 1,
+        loop: true,
         dots: true,
-        arrows: false,
+        dotsEach: true,
+        // autoWidth: true,
+        autoplay: true,
+        center: true,
     });
 }
 
